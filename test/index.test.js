344bf5029402201285ff5349d9ba4acfc5e0c7fd
zaytsev5 <zaytsev@99>
2021-06-19 14:31:36 +0700

    fix and add

--- test/index.test.js
+++ test/index.test.js
@@ -6,6 +6,9 @@ describe('sort an array', function() {
         assert.deepEqual([1,10000,21,30,4].sort(descSort),[10000,30,21,4,1])
     });
     it('should sorted array orderd by ASC', function() {
-        assert.deepEqual([1,10000,21,30,4].sort(ascSort),[1,4,1,30,10000])
+        assert.deepEqual([1,10000,21,30,4].sort(ascSort),[1,4,21,30,10000])
     });
+    it('should sorted array orderd by ASC', function() {
+      assert.deepEqual([5,-10].sort(asccSort),[-10,5])
+  });
 });
