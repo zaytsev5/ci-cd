709cc8541dd46af3f3bf9fc7b87a0f9542f0bf89
zaytsev5 <zaytsev@99>
2021-06-19 13:39:31 +0700

    recommit

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+const { descSort, ascSort } = require('../src/index')
+const assert = require('assert')
+
+describe('sort an array', function() {
+    it('should sorted array orderd by DESC', function() {
+        assert.deepEqual([1,10000,21,30,4].sort(descSort),[10000,30,21,4,1])
+    });
+    it('should sorted array orderd by ASC', function() {
+        assert.deepEqual([1,10000,21,30,4].sort(ascSort),[1,4,21,30,10000])
+    });
+});
