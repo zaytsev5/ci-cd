73ddea25cb326963f16398408ca78c87232d9abd
zaytsev5 <zaytsev@99>
2021-07-16 22:31:05 +0700

    fix: ci/cd

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,6 @@ const assert = require('assert')
 
 describe('sort an array', function() {
     it('should sorted array orderd by DESC', function() {
-        assert.deepEqual([1,10000,21,30,4].sort(descSort),[10000,30,21,4,1])
+        assert.deepEqual([10000,21,30,4].sort(descSort),[10000,30,21,4])
     }); 
 });
